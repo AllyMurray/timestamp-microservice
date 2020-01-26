b4682bf06959fa44eaa5239018e3d5c95acaeb3d
Ally Murray <allymurray88@gmail.com>
2020-01-26 16:22:22 +0000

    Eslint and jest set up

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true
+  },
+  extends: 'eslint:recommended',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {
+    eqeqeq: 'error',
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'no-console': 0
+  }
+};
