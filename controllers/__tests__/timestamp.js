5cb168755bfe231d77ccf1ad61861ad0ee8720c2
Ally Murray <allymurray88@gmail.com>
2020-01-28 08:32:21 +0000

    Handle unix timestamp

--- controllers/__tests__/timestamp.js
+++ controllers/__tests__/timestamp.js
@@ -5,7 +5,7 @@ const api = supertest(app);
 const apiUrl = '/api/timestamp';
 
 describe('when the timestamp endpoint is called', () => {
-  test('the response is returned as json', async () => {
+  test('a json response is returned', async () => {
     await api
       .get(`${apiUrl}/2020-01-01`)
       .expect(200)
@@ -13,37 +13,32 @@ describe('when the timestamp endpoint is called', () => {
   });
 });
 
-describe('when the timestamp endpoint is called with an empty date string', () => {
+describe('when the timestamp endpoint is called with an empty string', () => {
   test('the current timestamp should be returned', async () => {
-    const result = await api
-      .get(apiUrl)
-      .expect(200)
-      .expect('Content-Type', /application\/json/);
-
     const now = new Date();
     const expectedResult = {
       unix: now.getTime(),
       utc: now.toUTCString()
     };
 
+    const result = await api
+      .get(apiUrl)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
     // Due to two dates being created, one in the endpoint and one
     // to test the end point the unix timestamps aren't identical.
     // Checking that the difference is less than 10 is small enough
     // that we can assume the current timestamp was returned.
     // There may be a better way of handling this.
-    expect(expectedResult.unix - result.body.unix).toBeLessThan(10);
+    expect(result.body.unix - expectedResult.unix).toBeLessThan(10);
     expect(result.body.utc).toBe(expectedResult.utc);
   });
 });
 
-describe('when the timestamp endpoint is called with a valid date string', () => {
-  test('the timestamp for the given string should be returned', async () => {
+describe('when the timestamp endpoint is called with a valid', () => {
+  test('ISO-8601 compliant date string, the timestamp for the given string should be returned', async () => {
     const testDateString = '2020-01-01';
-
-    const result = await api
-      .get(`${apiUrl}/${testDateString}`)
-      .expect(200)
-      .expect('Content-Type', /application\/json/);
 
     const testDate = new Date(testDateString);
     const expectedResult = {
@@ -51,21 +46,43 @@ describe('when the timestamp endpoint is called with a valid date string', () =>
       utc: testDate.toUTCString()
     };
 
+    const result = await api
+      .get(`${apiUrl}/${testDateString}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    expect(result.body).toEqual(expectedResult);
+  });
+
+  test('unix timestamp string, the timestamp for the given string should be returned', async () => {
+    const testDateString = '1577836800000';
+
+    const testDate = new Date(Number(testDateString));
+    const expectedResult = {
+      unix: testDate.getTime(),
+      utc: testDate.toUTCString()
+    };
+
+    const result = await api
+      .get(`${apiUrl}/${testDateString}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
     expect(result.body).toEqual(expectedResult);
   });
 });
 
-describe('when the timestamp endpoint is called with a invalid date string', () => {
+describe('when the timestamp endpoint is called with a invalid string', () => {
   test('the unix timestamp should be null and the utc string should contain "Invalid Date"', async () => {
-    const result = await api
-      .get(`${apiUrl}/invalid-string`)
-      .expect(200)
-      .expect('Content-Type', /application\/json/);
-
     const expectedResult = {
       unix: null,
       utc: 'Invalid Date'
     };
+
+    const result = await api
+      .get(`${apiUrl}/invalid-string`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
 
     expect(result.body).toEqual(expectedResult);
   });
--- controllers/timestamp.js
+++ controllers/timestamp.js
@@ -2,7 +2,9 @@ const timestampRouter = require('express').Router();
 
 timestampRouter.get('/:dateString?', (request, response) => {
   const { dateString } = request.params;
-  const date = dateString ? new Date(dateString) : new Date();
+  const date = dateString
+    ? new Date(Number(dateString) || dateString)
+    : new Date();
   response.json({ unix: date.getTime(), utc: date.toUTCString() });
 });
 
